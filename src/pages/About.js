c55e93b658261f322c534f64ebfb63480af8572b
oNUR EGELİ <onuregeli243@icloud.com>
2023-04-03 21:49:38 +0300

    commit

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h2 className='section-title'>about us</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum autem ab soluta dolore animi commodi labore quos asperiores! Neque nesciunt in, deserunt dolor id repellendus, vel incidunt repellat adipisci quia, hic fugiat. Hic voluptatum minima distinctio dolor doloribus quas asperiores.
+      </p>
+    </section>
+  )
+}
+
+export default About
